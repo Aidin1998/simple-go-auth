b00d6918229729ac0fa8f629a9ac95c349362142
Aidin98 <aidin.montazami@gmail.com>
2025-04-27 16:09:48 +1000

    update

--- auth/validation.go
+++ auth/validation.go
@@ -3,20 +3,18 @@ package auth
 import (
 	"errors"
 	"regexp"
-	"strings"
 )
 
 func ValidateSignUpInput(email, password string) error {
-	// Validate email format
-	emailRegex := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
-	matched, _ := regexp.MatchString(emailRegex, email)
-	if !matched {
+	// Stronger email validation
+	emailRegex := `^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$`
+	re := regexp.MustCompile(emailRegex)
+
+	if !re.MatchString(email) {
 		return errors.New("invalid email format")
 	}
-	if !strings.Contains(email, "@") {
-		return errors.New("invalid email format")
-	}
-	// Validate password (example: minimum 8 characters)
+
+	// Validate password length
 	if len(password) < 8 {
 		return errors.New("password must be at least 8 characters long")
 	}
